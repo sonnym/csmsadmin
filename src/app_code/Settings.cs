452762067e93d4ceadfe6e563e24f308105c6961
Sonny Michaud <evissecer@wander.site>
2010-04-25 23:12:53 -0400

    created a new folder for screenshots and added one for the login screen; added links for editing permissions

--- src/app_code/Settings.cs
+++ src/app_code/Settings.cs
@@ -2,7 +2,8 @@ public class Settings {
 	public static string Version = "0.4.1";
 
 	// master connection string - required to access database before login unless AllowArbitraryConnectionStrings is true below
-	public static string ConnectionString = @"";
+	// public static string ConnectionString = "";
+	public static string ConnectionString = @"Server=.\SQLEXPRESS;Trusted_Connection=True;MultipleActiveResultSets=True;Timeout=60;";
 
 	// security options
 	public static bool DisableLoginPage = false;
